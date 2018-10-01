4c2478fda0423b3bd6be9f5f12cf1719a2b3cb24
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-10-01 17:01:04 +0200

    layout

--- assets/imagehover.js
+++ assets/imagehover.js
@@ -1,4 +1,4 @@
-jQuery("img").wrap('<div class="alt-wrap"/>');
-jQuery("img").each(function() {
+jQuery("img:not(#footer)").wrap('<div class="alt-wrap"/>');
+jQuery("img:not(#footer)").each(function() {
     jQuery(this).after('<p class="alt">' + jQuery(this).attr('alt') + '</p>');
 });
