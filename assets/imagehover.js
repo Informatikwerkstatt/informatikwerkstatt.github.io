2dac897c8ec50f5988052a5f4de839788acdd739
Philipp Kraus <philipp.kraus@flashpixx.de>
2018-10-01 17:06:45 +0200

    not selector

--- assets/imagehover.js
+++ assets/imagehover.js
@@ -1,4 +1,4 @@
-jQuery("img:not(#footer)").wrap('<div class="alt-wrap"/>');
-jQuery("img:not(#footer)").each(function() {
+jQuery("img:not(#footer img)").wrap('<div class="alt-wrap"/>');
+jQuery("img:not(#footer img)").each(function() {
     jQuery(this).after('<p class="alt">' + jQuery(this).attr('alt') + '</p>');
 });
